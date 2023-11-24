79486234ef1a293d3bcccf45c8a45481c5569209
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2023-11-24 18:13:48 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+//prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("copied the text:" + link)
+    } catch (err){
+        console.log(err)
+    }
+}
+
+shareButtons.forEach(shareButtons => shareButtons.addEventListener('click' , copyText))
