9cdd06fc35f4f6e5c9867dd30c3ee164bf16c836
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-05-17 16:30:31 +0000

    Secciones para los links

--- src/front/js/component/linktree .js
+++ src/front/js/component/linktree .js
@@ -6,9 +6,15 @@ import profile from '../component/images/profileImg.jpg';
 export const LinkTree = () => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row d-flex flex-column">
                 <div className="col d-flex justify-content-center align-items-center mt-5">
                     <img src={profile} className="img-Item" alt="..." />
+                </div>
+                <div className="col col d-flex justify-content-center align-items-center mt-5">
+                    <h1> aqui van tus links</h1>
+                </div>
+                <div className="col col d-flex justify-content-center align-items-center mt-5">
+                    <h1> aqui van tus links</h1>
                 </div>
             </div>
         </div>
