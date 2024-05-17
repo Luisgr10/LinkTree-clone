85e7504e2212ac0b18e8801ff87212b457353b7f
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-05-17 15:49:21 +0000

    Elimino componentes sin usar y creo nuevo componente para almacenar la foto

---
+++ src/front/js/component/linktree .js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export const LinkTree = () => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                <img src="..." class="img-fluid" alt="..."/>
+                </div>
+            </div>
+        </div>
+    )
+}
