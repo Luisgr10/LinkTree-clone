142a03f94eeb323ac27c94cdb3daea9053fac779
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-05-17 16:04:53 +0000

    Creo carpeta para css

--- src/front/js/component/linktree .js
+++ src/front/js/component/linktree .js
@@ -1,15 +1,16 @@
 import React from "react";
+import '../../styles/linktree.css'
+import profile from '../component/images/profileImg.jpg';
 
 
 export const LinkTree = () => {
-
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
-                <img src="..." class="img-fluid" alt="..."/>
+                    <img src={profile} className="img-fluid" alt="..." />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
