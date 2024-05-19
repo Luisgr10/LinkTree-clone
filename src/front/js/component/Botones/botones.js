bab377f42ac25a112db1c9cda88680b6ddce5e3f
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-05-19 16:44:25 +0000

    Mejoro eventos de botones

--- src/front/js/component/Botones/botones.js
+++ src/front/js/component/Botones/botones.js
@@ -3,16 +3,19 @@ import '../Botones/botones.css'
 
 export const Button = ({url,name,icon}) => {
 
-    const handleClick = () => {
+    const handleClick = (event) => {
+        event.preventDefault(); // Evita que se ejecute el enlace principal
+        event.stopPropagation(); // Evita que el evento se propague al enlace principal
         console.log('HICISTE CLICK');
     }
+
     return (
-        <button className="button">
-           <span>{icon}</span>  <a href={url} target="_blank" rel="noopener noreferrer">{name}</a> 
-           <button 
+        <a href={url} target="_blank" rel="noopener noreferrer" className="button">
+           <span>{icon}</span>  <span>{name}</span> 
+           <span
            className="share-btn"
            onClick={handleClick}
-           ><i class="fa-solid fa-ellipsis fa-sm"></i></button>
-</button>
+           ><i class="fa-solid fa-ellipsis fa-sm"></i></span>
+</a>
     )
 }
--- src/front/js/component/linktree .js
+++ src/front/js/component/linktree .js
@@ -11,14 +11,18 @@ export const LinkTree = () => {
                 <div className="col d-flex justify-content-center align-items-center mt-5">
                     <img src={profile} className="img-Item" alt="..." />
                 </div>
-                <div className="col col d-flex justify-content-center align-items-center mt-5">
+                <div className="titles mt-5">
+                    <h1>Luis Guilarte Rodriguez</h1>
+                    <p>"Sin caos no hay evolución y sin evolución no hay cambios"</p>
+                </div>
+                <div className="col col d-flex justify-content-center align-items-center mt-3">
                     <Button
                     icon= {<i class="fa-brands fa-linkedin-in fa-2xl"></i>}
                     name='LinkedIn'
                     url='https://www.linkedin.com/in/luisgr10/'
-                    share={<i class="fa-solid fa-ellipsis fa-xl"></i>} />
+                     />
                 </div>
-                <div className="col col d-flex justify-content-center align-items-center mt-5">
+                <div className="col col d-flex justify-content-center align-items-center mt-3">
                     <Button
                     icon= {<i className="fa-brands fa-github fa-2xl"></i>}
                     name='Github'
