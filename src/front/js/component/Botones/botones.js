87d021e4b49354435cc056d1abb9635736be0636
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-05-19 20:05:21 +0000

    Agrego modal y le doy estilo

--- src/front/js/component/Botones/botones.js
+++ src/front/js/component/Botones/botones.js
@@ -1,16 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import '../Botones/botones.css'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 export const Buttons = ({ url, name, icon }) => {
-    const [show, setShow] = useState(false);
+    const [smShow, setSmShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = (event) => {
         event.preventDefault(); // Evita que se ejecute el enlace principal
         event.stopPropagation(); // Evita que el evento se propague al enlace principal
-        setShow(true);
+        setSmShow(true);
         console.log('HICISTE CLICK');
     }
 
@@ -25,9 +25,9 @@ export const Buttons = ({ url, name, icon }) => {
                 break;
             default:
                 navigator.clipboard.writeText(url)
-                .then(() => alert('Enlace copiado al portapapeles'))
-                .catch(err => console.error('Error al copiar al portapapeles:', err))
-            return;
+                    .then(() => alert('Enlace copiado al portapapeles'))
+                    .catch(err => console.error('Error al copiar al portapapeles:', err))
+                return;
         }
         window.open(shareUrl, '_blank');
     }
@@ -42,26 +42,25 @@ export const Buttons = ({ url, name, icon }) => {
                 ><i class="fa-solid fa-ellipsis fa-sm"></i></span>
             </a>
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal size="sm"
+                show={smShow}
+                onHide={() => setSmShow(false)}
+                aria-labelledby="example-modal-sizes-title-sm"
+                centered>
                 <Modal.Header closeButton>
-                    <Modal.Title>Compartir enlaces</Modal.Title>
+                    <h5 className="m-title">Compartir enlaces</h5>
                 </Modal.Header>
-                <Modal.Body>
-                    <Button variant="primary" onClick={() => handleShare('whatsapp')}>
-                        Compartir en WhatsApp
-                    </Button>
-                    <Button variant="primary" onClick={() => handleShare('linkedin')}>
-                        Compartir en LinkedIn
-                    </Button>
-                    <Button variant="primary" onClick={() => handleShare('clipboard')}>
-                        Copiar al portapapeles
-                    </Button>
+                <Modal.Body className="m-body">
+                    <button className="btn-modal my-1" onClick={() => handleShare('whatsapp')}>
+                        <i class="fa-brands fa-whatsapp fa-lg"></i> Compartir en WhatsApp
+                    </button>
+                    <button className="btn-modal my-1" onClick={() => handleShare('linkedin')}>
+                        <i class="fa-brands fa-linkedin-in fa-lg"></i> Compartir en LinkedIn
+                    </button>
+                    <button className="btn-modal my-1" onClick={() => handleShare('clipboard')}>
+                        <i class="fa-regular fa-clipboard fa-lg"></i> Copiar al portapapeles
+                    </button>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Cerrar
-                    </Button>
-                </Modal.Footer>
             </Modal>
         </div>
 
